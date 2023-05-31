6e1583b39e6c34c33f00d4c17ed4d6c5fe95853e
Everett Vergara <evergara@shinra.com.ph>
2023-05-31 19:34:30 +0800

    added identity test

--- test_identity.cpp
+++ test_identity.cpp
@@ -7,7 +7,11 @@ auto main(int, char *[]) -> int
     eg::identity tb_sys_mf_user_id("tb_sys_mf_user");
 
     auto next = tb_sys_mf_user_id.get_next_id();
-    std::cout << next << std::endl;
+    auto curr = tb_sys_mf_user_id.get_curr_id();
+
+    std::cout << next << " " << curr << std::endl;
+
+
 
     return 0;
 }
