3e4f67fc24579d9772e4117fb720f45478a53b15
Everett Vergara <evergara@shinra.com.ph>
2023-06-02 19:25:37 +0800

    added block_data

---
+++ include/block_data.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <fstream>
+
+#include "common.hpp"
+
+namespace eg
+{
+
+    struct block_data
+    {
+
+        
+
+        auto get_size()
+        {
+            auto s = sizeof(*this);
+
+
+            return s;
+        }
+    };
+
+    
+
+}
