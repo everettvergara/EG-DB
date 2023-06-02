af2f76b6f0fb73f93ec65c263f369442bf34d306
Everett Vergara <evergara@shinra.com.ph>
2023-06-02 20:01:21 +0800

    completed load_block_data()

--- include/block_data.hpp
+++ include/block_data.hpp
@@ -29,14 +29,33 @@ namespace eg
         
         file.seekp(i * sizeof(T), std::ios::end);
         file.write(reinterpret_cast<const char *>(&data), sizeof(T));
-        if (file.fail()) throw std::runtime_error("Could not write to the file block size.");
+        if (file.fail()) throw std::runtime_error("Could not write to the file block.");
     }
 
     template <typename T>
     auto load_block_data(const std::string &filename, const uint_t i) -> T
     {
-        T data;
+        // Assumes file already exists, even if it's 0 bytes.
+        // Otherwise it will fail.
+        std::ifstream file(filename, std::ios::binary);
+        if (not file) throw std::runtime_error("Could not open the block file.");
 
+        // Check if the file has the right size
+        file.seekg(0, std::ios::end);
+        if (file.fail()) throw std::runtime_error("Could not get the file block size.");
+
+        // Check if version is compatible
+        auto file_size = file.tellg();
+        if (file_size % sizeof(T) not_eq 0) throw std::runtime_error("Version of block size is incompatible.");
+        
+        // Check if i is within the range
+        auto last_i = file_size / sizeof(T) - 1;
+        if (i > last_i) throw std::runtime_error("Invalid index to load.");
+
+        T data;
+        file.seekg(i * sizeof(T), std::ios::beg);
+        file.read(reinterpret_cast<char *>(&data), sizeof(T));
+        if (file.fail()) throw std::runtime_error("Could not read to the file block.");
 
         return data;
 
