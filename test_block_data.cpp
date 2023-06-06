70b3f57af1427e4c29327debc782829b0dd76960
Everett Vergara <evergara@shinra.com.ph>
2023-06-06 18:02:06 +0800

    misc test

--- test_block_data.cpp
+++ test_block_data.cpp
@@ -19,16 +19,30 @@ auto main(int, char *[]) -> int
         data_sample data {.x = 100, .y = 100};
         write_block_data<data_sample>("block_data_sample.data", data, 1);
 
+        // Should show 0, because it is skipped 
+        
         auto data1 = read_block_data<data_sample>("block_data_sample.data", 0);
         std::cout << "data1." << data1.x << std::endl;    
 
+        
+        // Should show 100 
         auto data2 = read_block_data<data_sample>("block_data_sample.data", 1);
         std::cout << "data2." << data2.x << std::endl;    
 
+
+        // Should show 200 
         data_sample data3 {.x = 200, .y = 200};
         write_block_data<data_sample>("block_data_sample.data", data3, 2);
         auto data33 = read_block_data<data_sample>("block_data_sample.data", 2);
         std::cout << "data33." << data33.x << std::endl;
+
+
+        // Should show 100 
+        auto data22 = read_block_data<data_sample>("block_data_sample.data", 1);
+        std::cout << "data22." << data22.x << std::endl;    
+
+
+        // Should throw an error 
         auto data44 = read_block_data<data_sample>("block_data_sample.data", 3);
         std::cout << "data44." << data44.x << std::endl;
 
