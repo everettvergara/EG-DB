b2b0642881d7a6a810d6aa624499aac0bae9f7ed
Everett Vergara <evergara@shinra.com.ph>
2023-06-06 19:54:05 +0800

    misc edits on block_data

--- include/block_data.hpp
+++ include/block_data.hpp
@@ -30,60 +30,68 @@ namespace eg
         return file;
     }
 
+    template<typename T>
+    auto get_file_size(std::fstream &file) -> std::streampos
+    {
+        file.seekg(0, std::ios::end);
+        if (file.fail()) throw std::runtime_error("Could not set the file stream loc.");
+
+        auto file_size = file.tellg();
+        if (file.fail()) throw std::runtime_error("Could not get the file stream loc.");
+
+        return file_size;
+    }
+
+
     template <typename T>
     auto write_block_data(std::fstream &file, const T &data, const uint64_t i)
     {
         static_assert(std::is_trivially_copyable_v<T>, "Datastruct too complex.");
 
-        // Check if the file has the right size
-        file.seekp(0, std::ios::end);
-        if (file.fail()) throw std::runtime_error("Could not get the file block size.");
-
-        // Check if version is compatible
-        auto file_size = file.tellp();
-        if (file_size % sizeof(T) not_eq 0) throw std::runtime_error("Version of block size is incompatible.");
-        
         file.seekp(i * sizeof(T), std::ios::beg);
         file.write(reinterpret_cast<const char *>(&data), sizeof(T));
         if (file.fail()) throw std::runtime_error("Could not write to the file block.");
     }
 
     template <typename T>
-    auto write_block_data(const std::string &filename, const T &data, const uint64_t i)
+    auto read_block_data(const std::fstream file, const uint64_t i) -> T
     {
-        static_assert(std::is_trivially_copyable_v<T>, "Datastruct too complex.");
-        
-        std::fstream file = get_file_handler_for_write_block_data(filename);
-        write_block_data<T>(file, data, i);
-    }
-
-    template <typename T>
-    auto read_block_data(const std::string &filename, const uint64_t i) -> T
-    {
-        static_assert(std::is_trivially_copyable_v<T>, "Datastruct too complex.");
-
-        // Assumes file already exists, even if it's 0 bytes.
-        // Otherwise it will fail.
-        std::ifstream file(filename, std::ios::binary);
-        if (not file) throw std::runtime_error("Could not open the block file.");
-
-        // Check if the file has the right size
-        file.seekg(0, std::ios::end);
-        if (file.fail()) throw std::runtime_error("Could not get the file block size.");
-
-        // Check if version is compatible
-        auto file_size = file.tellg();
-        if (file_size % sizeof(T) not_eq 0) throw std::runtime_error("Version of block size is incompatible.");
-        
-        // Check if i is within the range
-        auto last_i = file_size / sizeof(T) - 1;
-        if (i > last_i) throw std::runtime_error("Invalid index to load.");
-
         T data;
         file.seekg(i * sizeof(T), std::ios::beg);
         file.read(reinterpret_cast<char *>(&data), sizeof(T));
-        if (file.fail()) throw std::runtime_error("Could not read to the file block.");
+        if (file.fail()) throw std::runtime_error("Could not read from the file block.");
 
-        return data;
+        return data;        
     }
+
+
+    // template <typename T>
+    // auto read_block_data(const std::string &filename, const uint64_t i) -> T
+    // {
+    //     static_assert(std::is_trivially_copyable_v<T>, "Datastruct too complex.");
+
+    //     // Assumes file already exists, even if it's 0 bytes.
+    //     // Otherwise it will fail.
+    //     std::ifstream file(filename, std::ios::binary);
+    //     if (not file) throw std::runtime_error("Could not open the block file.");
+
+    //     // Check if the file has the right size
+    //     file.seekg(0, std::ios::end);
+    //     if (file.fail()) throw std::runtime_error("Could not get the file block size.");
+
+    //     // Check if version is compatible
+    //     auto file_size = file.tellg();
+    //     if (file_size % sizeof(T) not_eq 0) throw std::runtime_error("Version of block size is incompatible.");
+        
+    //     // Check if i is within the range
+    //     auto last_i = file_size / sizeof(T) - 1;
+    //     if (i > last_i) throw std::runtime_error("Invalid index to load.");
+
+    //     T data;
+    //     file.seekg(i * sizeof(T), std::ios::beg);
+    //     file.read(reinterpret_cast<char *>(&data), sizeof(T));
+    //     if (file.fail()) throw std::runtime_error("Could not read to the file block.");
+
+    //     return data;
+    // }
 }
