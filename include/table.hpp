1ea3578f12a85e8784af366fba8d57284e4ad7c6
Everett Vergara <evergara@shinra.com.ph>
2023-06-12 10:52:20 +0800

    revised table

--- include/table.hpp
+++ include/table.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 #include "column_attr.hpp"
 #include "column.hpp"
-#include "row.hpp"
-#include "identity.hpp"
+// #include "row.hpp"
+// #include "page_ix.hpp"
 
 namespace eg
 {
@@ -13,17 +14,22 @@ namespace eg
     {
     public:
 
-        table(const std::string &name)
-            : name_(name), id_(name_)
+        table(const std::string &table_name, std::vector<column> &&columns)
+            :   table_name_(table_name), 
+                page_ix_name_(table_name_ + ".ix"),
+                columns_(std::move(columns))
         {
+            for (uint_t i = 0; auto c : columns_)
+                column_cardinality_.try_emplace(c.get_name(), i++); 
         }
 
-        
     private:
 
-        std::string name_;
-        identity    id_;
-
+        std::string table_name_;
+        std::string page_ix_name_;
+        
+        std::vector<column> columns_;
+        std::unordered_map<const char *, uint_t> column_cardinality_;
     };
-
 }
+
